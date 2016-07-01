4b2184b5d27c5e24c278c7e5e4bbad6cc4ee183e
Noah <yasskin@icloud.com>
2016-07-01 09:27:17 -0700

    add pecker_picker() method that returns a count of a given input

--- lib/pecker_picker.rb
+++ lib/pecker_picker.rb
@@ -1,6 +1,13 @@
 class String
   define_method(:pecker_picker) do |pick|
+    prose_input = self
+    counter = 0
 
-
+    split_words = prose_input.split(' ')
+    split_words.each
+    if split_words = pick
+      counter =+ 1
+    end
+    return counter
   end
 end
