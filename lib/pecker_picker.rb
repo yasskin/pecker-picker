898d95836f02987e936c43127eeafc7601a49b16
Noah <yasskin@icloud.com>
2016-07-01 09:35:23 -0700

    add several tests that pass pecker picker

--- lib/pecker_picker.rb
+++ lib/pecker_picker.rb
@@ -2,11 +2,14 @@ class String
   define_method(:pecker_picker) do |pick|
     prose_input = self
     counter = 0
+    word = pick
 
     split_words = prose_input.split(' ')
-    split_words.each
-    if split_words = pick
-      counter =+ 1
+
+    split_words.each do |word|
+      if word == pick
+        counter += 1
+      end
     end
     return counter
   end
--- spec/pecker_spec.rb
+++ spec/pecker_spec.rb
@@ -4,7 +4,19 @@ require('./lib/pecker_picker.rb')
 describe('String#pecker_picker') do
 
   it("tells the user if their first string input appears in their second string intput") do
-    expect("a".pecker_picker("a")).to(eq(1))
+    expect("p".pecker_picker("p")).to(eq(1))
+  end
+
+  it("tells the user how many times their selected word appears when given the same word") do
+    expect("peter".pecker_picker("peter")).to(eq(1))
+  end
+
+  it("tells the user how many times their selected word appears in another string intput with multiple words") do
+    expect("peter piper picked a pack".pecker_picker("peter")).to(eq(1))
+  end
+
+  it("tells the user how many times their selected word appears in another string intput with multiple words when the word appears more than once") do
+    expect("peter piper picked a pack of pickele peppers. how many peppers did peter piper pick?".pecker_picker("peter")).to(eq(2))
   end
 
 end
